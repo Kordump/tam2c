c678b425e08d293c3f798a290c695275cab64a21
Matthieu Daumas <daumasm@bde.enseeiht.fr>
2016-04-08 08:59:58 +0200

    Add argc checking.

---
+++ tam2c.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    if(argc != 2)
+    {
+        std::cout <<
+            "Usage: " << argv[0] << " <filename>.tam\n"
+            " - Traduce TAM assembly in C using tamlibc.\n"
+            " - Will output \"<filename>.tam.c\" in the same directory."
+            << std::endl;
+        exit(1);
+    }
+
+    return 0;
+}
