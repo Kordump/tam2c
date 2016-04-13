682ab500f3307c463dea6637948af95e8debf820
Matthieu Daumas <daumasm@bde.enseeiht.fr>
2016-04-13 17:02:18 +0200

    Add grammar to parse one line of a TAM file.

--- tam2c.hpp
+++ tam2c.hpp
@@ -20,11 +20,92 @@ namespace tam2c
 {
     namespace grammar
     {
-
+        // Grammar to parse **one line** of TAM
         using namespace pegtl;
 
+        // Matching ';' implies consuming tokens until eolf.
         struct comment
             : if_must<one<';'>, until<eolf>>
+            { };
+
+        // Match a number, decimal or hexadecimal.
+        struct number
+            : sor<plus<digit>, seq<string<'0', x>, plus<digit>>>
+            { };
+
+        // Match a label, from 1 to 255 alphanumeric chars.
+        struct label
+            : rep_min_max<1, 255, alpha>
+            { };
+
+        // Match numbers wrapped in their syntax. (raw, (raw), raw[raw])
+        struct atomic_number
+            : sor<
+                number,
+                seq<one<'('>, number, one<')'>>,
+                seq<number, one<'['>, number, one<']'>>>
+            { };
+
+        // Match labels wrapped in string semantics. (raw, 'raw', "raw")
+        struct atomic_label
+            : sor<
+                label,
+                seq<one<'\"'>, label, one<'\"'>>,
+                seq<one<'\''>, label, one<'\''>>>
+            { };
+
+        // Match legal instruction names.
+        struct inst_name
+            : sor<
+                string<P, U, S, H>,
+                string<P, O, P>,
+                string<L, O, A, D, L>,
+                string<L, O, A, D, A>,
+                string<L, O, A, D, I>,
+                string<L, O, A, D>,
+                string<S, T, O, R, E, I>,
+                string<S, T, O, R, E>,
+                string<J, U, M, P, I, F>,
+                string<J, U, M, P>,
+                string<S, U, B, R>,
+                string<H, A, L, T>
+              >
+            { };
+
+        // Match the generic structure of a TAM instruction.
+        struct inst_generic
+            : if_must<
+                inst_name,
+                rep_opt<2, pad<
+                    sor<atomic_number, atomic_label>,
+                    space>>>
+            { };
+
+        // Match a label definition.
+        struct define_label
+            : pad<
+                seq<label, one<':'>>,
+                space>
+            { };
+
+        // Match an instruction, a label definition, or the combination.
+        struct inst
+            : sor<
+                inst_generic,
+                define_label,
+                if_must<
+                    define_label,
+                    pad<inst_generic, space>>>
+            { } ;
+
+        // Match a line, empty, commented, or the combination.
+        struct line
+            : must<
+                sor<
+                    seq<pad<inst, space>, opt<comment>>,
+                    pad<comment, space>,
+                    seq<star<space>, eof>>
+              >
             { };
     }
 
