e703adb7fe3bb442f8d69c63ac4b6f0e004903d8
Matthieu Daumas <daumasm@bde.enseeiht.fr>
2016-04-13 16:56:59 +0200

    Add code to read the file line by line.

--- tam2c.cpp
+++ tam2c.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
 #include "tam2c.hpp"
 
 int main(int argc, char* argv[])
@@ -11,6 +14,13 @@ int main(int argc, char* argv[])
             " - Will output \"<filename>.tam.c\" in the same directory."
             << std::endl;
         exit(1);
+    }
+
+    size_t nb_line = 0;
+    std::ifstream tam_file(argv[1]);
+    for(std::string line = ""; std::getline(tam_file, line); ++nb_line)
+    {
+        std::cout << line << "\n";
     }
 
     return 0;
