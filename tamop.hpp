45a7133ba8232e41c698108058cac025b35fabc5
Matthieu Daumas <daumasm@bde.enseeiht.fr>
2016-04-15 20:24:40 +0200

    Move generic tamop to tamop_generic.hpp .

--- tamop.hpp
+++ tamop.hpp
@@ -6,49 +6,6 @@
 
 namespace tam2c
 {
-    template<typename... t_op>
-    struct opcode_details
-    {
-        template<typename t_this>
-        struct wrap
-        {
-            using grammar = typename t_this::grammar;
-        };
-
-        using grammar = std::tuple<typename wrap<t_op>::grammar...>;
-        static const size_t cardinal = sizeof...(t_op);
-
-        template<size_t t_opcode, typename t_match, typename... t_remaining>
-        struct match_details
-        {
-            static std::unique_ptr<instruction> name(std::string of_this)
-            {
-                if(of_this == op<t_match>::name)
-                    return std::make_unique<op<t_match>>();
-
-                return
-                    match_details<t_opcode - 1, t_remaining...>::name(of_this);
-            }
-        };
-
-        template<typename t_match, typename... t_remaining>
-        struct match_details<1, t_match, t_remaining...>
-        {
-            static std::unique_ptr<instruction> name(std::string of_this)
-            {
-                if(of_this == op<t_match>::name)
-                    return std::make_unique<op<t_match>>();
-
-                return std::make_unique<op<instruction::invalid>>();
-            }
-        };
-
-        static std::unique_ptr<instruction> match(std::string name)
-        {
-            return match_details<cardinal, t_op...>::name(name);
-        };
-    };
-
     // Implemented
 
     // Working on...
